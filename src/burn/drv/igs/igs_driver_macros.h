15a9d5fb0eef37bf89aefd4f7c34e587657c88a4
2494654220 <3774005520@qq.com>
2026-04-30 20:41:10 +0800

    123

--- src/burn/drv/igs/igs_driver_macros.h
+++
@@ -1,8 +0,0 @@
-#ifndef IGS_DRIVER_MACROS_H
-#define IGS_DRIVER_MACROS_H
-
-#define ROM_NAME(Name) NULL, Name##RomInfo, Name##RomName, NULL, NULL, NULL, NULL
-#define INPUT_FN(Name) Name##InputInfo
-#define DIP_FN(Name) Name##DIPInfo
-
-#endif
